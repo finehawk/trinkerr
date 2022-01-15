cb74f5ac1e27e66facffeb3bba3bc136630e8dc1
Nimish Khokhar <nimishx97@gmail.com>
2022-01-15 11:31:35 +0530

    minor bug

--- problem2.js
+++ problem2.js
@@ -1,4 +1,4 @@
-let input = "abcd";
+let input = "abcbccd";
 const unwant = "bc";
 
 // Output: "dab"
@@ -7,10 +7,9 @@ let isUnwantRemoved = false;
 
 while(!isUnwantRemoved) {
     let unwantRemove = false;
-    console.log(input);
     for(let i =0; i<input.length; i++) {
         if(input.charAt(i) == unwant.charAt(0)) {
-            if(unwant.length>1 && input.length-i-1>0) {
+                        if(unwant.length>1 && input.length-i-1>0) {
                 for(let j = 1; j<unwant.length; j++) {
                     if(i + j <= input.length-1) {
                         if(unwant.charAt(j) == input.charAt(i+j));
@@ -19,7 +18,9 @@ while(!isUnwantRemoved) {
                         break;
                     }
                     if(i+j+1 <= input.length-1) {
-                        input = input.substring(0,i-1) +  input.substring(i+j+1,input.length-1);
+                        // console.log(i + " " +  i+j+1);
+                        input = input.substring(0,i) +  input.substring(i+j+1,input.length);
+                        // console.log(input);
                     } else {
                         input = input.substring(0,i-1)
                     }
@@ -30,6 +31,10 @@ while(!isUnwantRemoved) {
         
         }
         if(unwantRemove) break;
-        if(i >= input.length-1) isUnwantRemoved = true;
+        if(i >= input.length-1){
+            
+        console.log(input);
+            isUnwantRemoved = true;
+        } 
     }
 }
